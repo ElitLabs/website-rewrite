ce621eaf92cb7e91b4738afc90ecd3dcbccc1674
Lior Pendler <lior.pendler@gmail.com>
2024-07-09 11:21:46 -0700

    Add theme provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,7 +7,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider attribute='class'>
 			<Toaster />
+			{children}
 		</ThemeProvider>
 	);
 }
-('use client');
